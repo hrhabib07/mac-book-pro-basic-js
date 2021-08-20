669b43a4b3fe01815ff1d1312e21a624c36e3439
hrhabib07 <futureofme07@gmail.com>
2021-08-21 03:32:35 +0600

    first commit

--- js/app.js
+++ js/app.js
@@ -1,10 +1,60 @@
+// code for memory section 
 document.getElementById('memory-increase').addEventListener('click', function(){
-    const memoryCost = document.getElementById('memory-cost');
-    const memoryPrice = 180;
-    memoryCost.innerText = memoryPrice;
-})
-document.getElementById('default-cost').addEventListener('click', function(){
-    const memoryCost = document.getElementById('memory-cost');
-    const memoryPrice = 0;
-    memoryCost.innerText = memoryPrice;
-})
+    changeValue('memory-cost', 180);
+    updateTotal();
+});
+document.getElementById('memory-default').addEventListener('click', function(){
+    changeValue('memory-cost', 0);
+    updateTotal();
+});
+
+
+// code for Storage section 
+
+document.getElementById('storage-default').addEventListener('click', function(){
+    changeValue('storage-cost', 0);
+    updateTotal();
+});
+document.getElementById('storage-increase').addEventListener('click', function(){
+    changeValue('storage-cost', 100);
+    updateTotal();
+});
+document.getElementById('storage-more-increase').addEventListener('click', function(){
+    changeValue('storage-cost', 180);
+    updateTotal();
+});
+
+// code for delivery cost
+
+document.getElementById('delivery-default').addEventListener('click', function(){
+    changeValue('delivery-cost', 0);
+    updateTotal();
+}); 
+document.getElementById('delivery-increase').addEventListener('click', function(){
+    changeValue('delivery-cost', 20);
+    updateTotal();
+});
+
+
+function changeValue(update ,price){
+    const cost = document.getElementById(update);
+    const productPrice = price;
+    cost.innerText = productPrice;   
+};
+
+// update total price 
+function updateTotal(){
+    const bestPrice =getPrice('best-cost');
+    const memoryPrice =getPrice('memory-cost');
+    const storagePrice =getPrice('storage-cost');
+    const deliveryPrice =getPrice('delivery-cost');
+   
+    const total = document.getElementById('total-cost')
+    total.innerText = bestPrice + memoryPrice + storagePrice + deliveryPrice;   
+   
+};
+function getPrice(item){
+    const selectText = document.getElementById(item);
+    const convertNumber = parseFloat(selectText.innerText);
+    return convertNumber;
+};
